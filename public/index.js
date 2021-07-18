090c574326ae60a5a04f2a5fc54c459cb2e06f22
andytheo <igbeinkutuandrew@yahoo.com>
2021-07-18 11:30:08 +0100

    Edited colours

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
 $(window).scroll(() => {
   $("nav").toggleClass("scrolled", $(this).scrollTop()>400);
 });
+
+
+function year() {
+  var year = new Date().getFullYear();
+  var copyright = document.querySelector(".year").innerHTML = year;
+  return copyright;
+}
